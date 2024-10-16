db20b5199850760fddf155b62e30d5fd1d00b07b
Xolelwa34 <ciliaswekile@gmail.com>
2024-10-16 06:27:14 -0700

    Minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Calculates the fewest number of operations needed
+    to result in exactly  n H characters in the file
+"""
+
+
+def minOperations(n):
+    """
+    Computes the minimum number
+    of operations for task Copy All and Paste
+
+    Args:
+        n: input number of chars
+        factor_list: List to save the operations
+    Return: Sum of ops
+    """
+    if n ==1
+        return 0
+    elif n % 2 == 0:
+        return n // 2 + 1
+    else:
+        return 0
