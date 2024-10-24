c9a7126308850e6228937532e77a1bd177660945
Xolelwa34 <ciliaswekile@gmail.com>
2024-10-24 10:27:40 +0000

    adding sortings

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,43 +6,44 @@ Log parsing script that reads input line by line and computes metrics.
 
 import sys
 
+if __name__ == "__main__":
 
-def log_parsing(line):
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+    file_size = [0]
+    count = 1
+
+    def print_stats():
+        '''
+        Prints file for every 10 loops
+        '''
+        print('File size: {}'.format(file_size[0]))
+
+        for code in sorted(status_codes.keys()):
+            if status_codes[code] != 0:
+                print('{}: {}'.format(code, status_codes[code]))
+
+    def parse_stdin(line):
+        '''
+        Checks the stdin for matches
+        '''
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            file_size[0] += int(word[-1])
+            status_code = int(word[-2])
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
+
     try:
-        parts = line.split('"')
-        ip_address = parts[0].strip()
-        date = parts[1].split()[0].strip('[')
-        status_code = int(parts[2].split()[1])
-        file_size = int(parts[2].split()[-1])
-        return ip_address, date, status_code, file_size
-    except IndexError:
-        return None
-
-def compute_metrics(log_lines):
-    """
-    Computes metrics based on the parsed log lines.
-
-    Args:
-        log_lines (list): List of log lines.
-
-    Returns:
-        dict: A dictionary containing computed metrics.
-    """
-    total_requests = 0
-    total_file_size = 0
-    unique_ips = set()
-
-    for line in log_lines:
-        parsed_data = parse_log_line(line)
-        if parsed_data:
-            ip_address, _, _, file_size = parsed_data
-            total_requests += 1
-            total_file_size += file_size
-            unique_ips.add(ip_address)
-
-    metrics = {
-        "total_requests": total_requests,
-        "total_file_size": total_file_size,
-        "unique_ips": len(unique_ips)
-    }
-    return metrics
+        for line in sys.stdin:
+            parse_stdin(line)
+            if count % 10 == 0:
+                print_stats()
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
