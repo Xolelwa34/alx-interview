3b578e880ba024be9928009e27a8d4064ea62693
Xolelwa34 <ciliaswekile@gmail.com>
2024-12-02 08:18:41 +0000

    Island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+defines island perimeter
+"""
+
+def island_perimeter(grid):
+    """
+    Returns the island perimeter
+    """
+perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0]) if rows > 0 else 0
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                perimeter += 4
+                if r > 0 and grid[r - 1][c] == 1:
+                    perimeter -= 2
+                if c > 0 and grid[r][c - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
+
