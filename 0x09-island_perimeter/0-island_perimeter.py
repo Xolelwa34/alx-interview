437b132396838aeb6cb5817c8ff0a22708f6c9bc
Xolelwa34 <ciliaswekile@gmail.com>
2024-12-04 17:47:10 +0000

    Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 """
-defines island perimeter
+Module for Islad perimeter function
 """
 
+
 def island_perimeter(grid):
+    """Returns the Island perimiter
+     grid represents water by 0 and land by 1.
+    Args:
+     Grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
     """
-    Returns the island perimeter
-    """
-perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0]) if rows > 0 else 0
+    rows = len(grid[0])
+    cols = len(grid)
+    ram = 0
+    size = 0
 
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == 1:
-                perimeter += 4
-                if r > 0 and grid[r - 1][c] == 1:
-                    perimeter -= 2
-                if c > 0 and grid[r][c - 1] == 1:
-                    perimeter -= 2
-
-    return perimeter
+    for i in range(cols):
+        for j in range(rows):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                ram += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    ram += 1
+    return size * 4 - ram * 2
 
